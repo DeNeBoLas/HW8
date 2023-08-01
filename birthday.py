92d272c0ee2f659f6a58face0c54526a84a0d874
DeNeBoLas <svitlana-ilchenko@ukr.net>
2023-08-01 15:24:08 +0300

    Update birthday.py
    fixed weekend

--- birthday.py
+++ birthday.py
@@ -23,19 +23,19 @@ import calendar
 
 test_users = [
     {"name": "Kate", "birthday": datetime(year=2012, month=8, day=2)},  # "02.08.2012"
-    {"name": "Alex", "birthday": datetime(year=2005, month=8, day=5)},  # "05.08.2005"
-    {"name": "Sem", "birthday": datetime(year=2002, month=8, day=12)},  # "12.08.2002"
+    {"name": "Alex", "birthday": datetime(year=2005, month=8, day=4)},  # "05.08.2005"
+    {"name": "Sem", "birthday": datetime(year=2002, month=8, day=7)},  # "12.08.2002"
     {
         "name": "Nataly",
-        "birthday": datetime(year=2000, month=8, day=15),
+        "birthday": datetime(year=2000, month=8, day=6),
     },  # "15.08.2000"
     {
         "name": "Nic",
         "birthday": datetime(year=2015, month=8, day=8),
     },  # "10.08.2015" Monday
-    {"name": "Max", "birthday": datetime(year=2001, month=7, day=31)},  # "31.07.2001"
+    {"name": "Max", "birthday": datetime(year=2001, month=8, day=5)},  # "31.07.2001"
     {"name": "Leo", "birthday": datetime(year=2005, month=8, day=1)},  # "01.08.2005"
-    {"name": "Bill", "birthday": datetime(year=2003, month=8, day=3)},  # "01.08.2005"
+    {"name": "Bill", "birthday": datetime(year=2003, month=8, day=3)},  # "03.08.2005"
     {"name": "Kim", "birthday": datetime(year=2000, month=8, day=2)},  # "01.08.2005"
 ]
 days_dict = {}  # data for keys
@@ -51,12 +51,15 @@ def get_birthdays_per_week(users: list) -> dict:
         birthdays_in_this_year = user["birthday"].replace(year=today.year)
 
         if today <= birthdays_in_this_year < next_week:
-            day_of_week = user["birthday"].weekday()
+            day_of_week = birthdays_in_this_year.weekday()
 
-            if birthdays_in_this_year.weekday() in [6, 7]:
-                day_of_week[0].append(user["name"])
-
-            days_dict[day_of_week].append(user["name"])
+            if (
+                birthdays_in_this_year.weekday() == 5
+                or birthdays_in_this_year.weekday() == 6
+            ):
+                days_dict[0].append(user["name"])
+            else:
+                days_dict[day_of_week].append(user["name"])
 
     out_days = [
         "Monday",
